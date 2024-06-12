432733f8ba77370500fcb110a0390b22c6ee2839
Tiago Gadonski Cordeiro <tiagogadonski@hotmail.com>
2024-06-12 11:23:40 -0300

    Add project files.

---
+++ Hubs/GameHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+public class GameHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task PlayCard(string user, string card)
+    {
+        await Clients.All.SendAsync("CardPlayed", user, card);
+    }
+}
